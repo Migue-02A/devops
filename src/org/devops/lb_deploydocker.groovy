f265906b151928a448ae0053d83ec53dc7fccdf8
Migue-02A <miguelangelgomez1402@gmail.com>
2024-09-20 15:24:30 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -5,8 +5,8 @@ def despliegueContenedor(projectName) {
     sh "docker rm ${projectName} || true"
     
     def networkName = env.NET_NAME ?: 'default_network' // Valor por defecto
-    println "Network name: ${networkName}"
+    println "Network name: ${NameNetwork}"
     
     sh "docker pull miguelgomez02/${projectName}"
-    sh "docker run -d --name ${projectName} --network=${networkName} -p 5174:5174 --user root miguelgomez02/${projectName}"
+    sh "docker run -d --name ${projectName} --network=${NameNetwork} -p 5174:5174 --user root miguelgomez02/${projectName}"
 }
