3394460deb8b41470df94c4f7d0851cb29229bc7
Migue-02A <miguelangelgomez1402@gmail.com>
2024-09-20 15:06:55 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,9 +1,9 @@
 package org.devops
 
-def cargarDockerHub(projectGitName){
+def cargarDockerHub(projectName){
     withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
         sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-        sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"
-        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}"
+        sh "docker tag ${projectName} ${env.DOCKERHUB_USERNAME}/${projectName}"
+        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectName}"
     }
 }
