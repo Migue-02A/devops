6f9bb2380e12444d8dafd8decc3ef048c994a83f
Migue-02A <miguelangelgomez1402@gmail.com>
2024-09-20 15:53:46 -0500

    Update lb_owasp.groovy

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -1,5 +1,5 @@
 package org.devops
 
 def analisisOWASP(){
-    sh "docker run --rm -v fullscan_react-test_OWASP:/zap/wrk/:rw --user root --network=${env.NET_NAME} -t edansama96/zap2docker-stable zap-full-scan.py -t http://${env.dominio}:5174/ -r fullscan_react-test_OWASP.html -I"
+    sh "docker run --rm -v fullscan_react-test_OWASP:/zap/wrk/:rw --user root --network=${env.NameNetwork} -t edansama96/zap2docker-stable zap-full-scan.py -t http://${env.dominio}:5174/ -r fullscan_react-test_OWASP.html -I"
 }
