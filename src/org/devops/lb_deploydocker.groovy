76e8513d1d0630a7755197cb0de206933fc73046
Migue-02A <miguelangelgomez1402@gmail.com>
2024-09-20 15:19:08 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,6 +1,12 @@
 package org.devops
 
 def despliegueContenedor(projectName) {
-    sh "docker pull miguelgomez02/react-test"
-    sh "docker run -d --name ${projectName} --network=${env.dominio} -p 5174:5174 --user root miguelgomez02/${projectName}"
+    sh "docker stop ${projectName} || true"
+    sh "docker rm ${projectName} || true"
+    
+    def networkName = env.NET_NAME ?: 'default_network' // Valor por defecto
+    println "Network name: ${networkName}"
+    
+    sh "docker pull miguelgomez02/${projectName}"
+    sh "docker run -d --name ${projectName} --network=${networkName} -p 5174:5174 --user root miguelgomez02/${projectName}"
 }
