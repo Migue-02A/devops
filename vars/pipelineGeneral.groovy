489efd8a251d040209b923b8ec024edba8b821aa
Migue-02A <miguelangelgomez1402@gmail.com>
2024-09-19 22:29:18 -0500

    Update pipelineGeneral.groovy

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,39 +1,42 @@
-def call(){
-    
+def call(Map param){
+
     pipeline{
 
         agent any
 
         tools{
-            nodejs 'NodeJS18'
+            nodejs 'NodeJS'
+        }
+
+        /* triggers {
+             pollSCM('* * * * *') // Programa la verificación del repositorio cada minuto
+        } */
+
+        environment{
+            PROJECT = "${env.GIT_URL}".replaceAll('.+/(.+)\\.git', '$1')toLowerCase()
+        }
+
+        stage('Construccion App') {
+            steps {
+                script {
+                    def buildapp = new org.devops.lb_buildartefacto()
+                    buildapp.install()
+                    def cloneapp = new org.devops.lb_buildartefacto()
+                    cloneapp.clone()
+                }
+            }
+        }
+
+        stage('Analisis Sonarqube') {
+            steps {
+                script {
+                    def test = new org.devops.lb_analisissonarqube()
+                    test.testCoverage()
+                    def analisisSonar = new org.devops.lb_analisissonarqube()
+                    analisisSonar.analisisSonar("$PROJECT")
+                }
+            }
         }
         
-        environment{
-            projectName = "${env.GIT_URL_1}".replaceAll('.+/(.+)\\.git', '$1')toLowerCase()
-        }
-
-        stages{
-            stage('Fase 1: Proceso de construcción') {
-                steps {
-                    script {
-                        def cloneapp = new org.devops.lb_buildartefacto()
-                        cloneapp.clone()
-                        def buildapp = new org.devops.lb_buildartefacto()
-                        buildapp.install()
-                    }
-                }
-            }
-
-            stage('Fase 1: Análisis de Sonarqube'){
-                steps{
-                    script{
-                        def test = new org.devops.lb_analisissonarqube()
-                        test.runTest()
-                        def analisysSonarqube = new org.devops.lb_analisissonarqube()
-                        analisysSonarqube.analisys("${projectName}")
-                    }
-                }
-            }
-        }
     }
 }
