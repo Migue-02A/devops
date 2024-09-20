20cadfbe7b80832e332ad96a505d89dabe194734
Migue-02A <miguelangelgomez1402@gmail.com>
2024-09-20 14:57:44 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,6 +1,8 @@
 package org.devops
 
 def despliegueContenedor(projectGitName){
+    sh "docker stop ${projectGitName}"
+    sh "docker rm ${projectGitName}"
     sh "docker pull miguelgomez02/react-test"
     sh "docker run -d --name ${projectGitName} --network=${env.NET_NAME} -p 5174:5174 --user root miguelgomez02/${projectGitName}"
 }
