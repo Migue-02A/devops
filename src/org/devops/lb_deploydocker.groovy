7a752b70b7e3835f88d35fd06407742e4a754fd6
Migue-02A <miguelangelgomez1402@gmail.com>
2024-09-20 15:17:33 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -2,5 +2,5 @@ package org.devops
 
 def despliegueContenedor(projectName) {
     sh "docker pull miguelgomez02/react-test"
-    sh "docker run -d --name ${projectName} --network=${env.NET_NAME} -p 5174:5174 --user root miguelgomez02/${projectName}"
+    sh "docker run -d --name ${projectName} --network=${env.dominio} -p 5174:5174 --user root miguelgomez02/${projectName}"
 }
